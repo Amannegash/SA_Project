de3e1ab89cb007958e97084515dd0c3ce23edd15
misghinna <75956497+misghinna@users.noreply.github.com>
2021-10-17 17:46:00 -0500

    Add files via upload

---
+++ eureka-server/eureka-server/src/main/java/edu/miu/c590/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package edu.miu.c590.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
